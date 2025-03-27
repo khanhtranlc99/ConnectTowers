de0c3ec658adad0fcde8411055831a5155fcb76e
Dung <vuquocdung204@gmail.com>
2025-03-27 17:45:23 +0700

    fix duoc roi dcm

--- Assets/_DungDev/09ScriptsVip/V_SlotCategory.cs
+++ Assets/_DungDev/09ScriptsVip/V_SlotCategory.cs
@@ -49,17 +49,12 @@ public class V_SlotCategory : MonoBehaviour
         this.HandleBtnState(!rewardCategory.isClaim);
         this.PostEvent(EventID.UPDATE_COIN_GEM);
         this.PostEvent(EventID.UPDATE_VIP_BOX);
-        this.UpdateUI(rewardCategory);
     }
 
     public void UpdateUI(V_RewardCategory rewardCategory)
     {
         //false het
         foreach (var child in this.lsItemSlots) child.gameObject.SetActive(false);
-        var dataVip = GameController.Instance.dataContain.dataUser.DataUserVip;
-
-        if (rewardCategory.LsRewardSlots.Count > 4) return;
-
         for (int i = 0; i < rewardCategory.LsRewardSlots.Count; i++)
         {
             this.lsItemSlots[i].gameObject.SetActive(true);
